e659e47911e229e25a896907c6bd1745b823dded
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2020-04-16 11:10:48 +0700

    execute file python

---
+++ first_app.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+address = input("where is your address? ")
+
+print("Hello " + name +" . You are from " + address)
---
+++ first_file.py
@@ -0,0 +1,7 @@
+# calling file python3 first_file.py //single comment
+
+"""
+multi line comment
+"""
+
+print('this is python file')
