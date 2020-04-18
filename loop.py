60ef86fe837eb8b49b799ecc7dc98e2e3d027747
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2020-04-18 11:45:42 +0700

    loop for in

--- loop.py
+++ loop.py
@@ -1,11 +1,22 @@
 """
 Loop
 - While
+- For In
 """
 
-count = 1
-while count < 5:
-    print("loop running")
-    count = count+1
-else:
-    print("loop stop, current count is", count)
+#While
+#count = 1
+#while count < 5:
+#    print("loop running")
+#    count = count+1
+#else:
+#    print("loop stop, current count is", count)
+
+texts = 'python'
+peoples = ['Jhon', 'Doe', 'Alex']
+
+for people in peoples:
+    print('People name is', people)
+
+for text in texts:
+    print('Alphabet ', text)
