7d07b07da251ce162ae84f3113e672a1b5e13078
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2020-04-24 06:59:06 +0700

    nested dictionary

--- dictionary.py
+++ dictionary.py
@@ -9,3 +9,16 @@ print(data['name'])
 #loop
 for key, value in data.items():
     print(key + " - " + value)
+
+nested_data = {
+    1: {
+        'name' : 'Suarez',
+        'number' : '9',
+    },
+    2: {
+        'name' : 'Griezmann',
+        'number' : '17'
+    }
+}
+
+print(nested_data[1])
