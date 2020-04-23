0fe79b828bca86d2a3369a52fe302b2728036cd8
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2020-04-23 21:46:29 +0700

    tuples

---
+++ tuples.py
@@ -0,0 +1,9 @@
+"""
+The different between list and tuples are :
+1. Tuples use () not []
+2. Tuples value can't add / edit / delete
+"""
+
+people = ('Neymar','Lautaro','Messi')
+
+print(people[0])
