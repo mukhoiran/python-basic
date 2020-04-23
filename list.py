fa5f4a2841754ace2d27012d67b6ac3234bcb27f
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2020-04-23 21:41:51 +0700

    list

---
+++ list.py
@@ -0,0 +1,18 @@
+people = ['Messi','Suarez','Griezmann']
+age = [32,33,29]
+mixed = ['argentina',17,'goal']
+
+# print out value
+print(people[0])
+
+# add new value
+people.append('Neymar')
+print(people)
+
+# change value
+people[2] = 'Dembele'
+print(people)
+
+# delete list
+del people[2]
+print(people)
