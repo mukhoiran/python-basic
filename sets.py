472acac91ea2a2fd9d4f318e6d37d800f106f93b
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2020-04-24 07:03:43 +0700

    sets

---
+++ sets.py
@@ -0,0 +1,10 @@
+people = {'football','messi','barcelona','football'}
+print(people)
+
+#sets will automatically remove duplicate value
+
+number1 = {1,2,3,4,5}
+number2 = {4,5,6,7,8}
+
+#do an operation or union
+print(number1 | number2)
